35d5debc8a351c11dcad6c1002920515acac6897
B-P-O <pauloluwadamilare912@gmail.com>
2025-10-29 14:10:56 +0100

    update user-service

--- user-service/cmd/api/healthcheck.go
+++ user-service/cmd/api/healthcheck.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"time"
 )
 
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	dbStatus := "up"
+	if err := app.models.Users.DB.PingContext(ctx); err != nil {
+		dbStatus = "down"
+	}
+
 	env := envelope{
 		"status": "available",
 		"system_info": map[string]string{
 			"environment": app.config.env,
 			"version":     version,
+			"database":    dbStatus,
 		},
 	}
 	err := app.writeJSON(w, http.StatusOK, env, nil)
