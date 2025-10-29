697a4bcc397e0ccba06d8993f7b9b00d3751e47b
B-P-O <pauloluwadamilare912@gmail.com>
2025-10-29 14:15:11 +0100

    update user-service

--- user-service/cmd/api/routes.go
+++ user-service/cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -18,17 +19,23 @@ func (app *application) routes() http.Handler {
 
 	router.MethodFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
+	//api endpoint needed by other services to interact with user-service
+	router.MethodFunc(http.MethodGet, "/v1/users/{id}", app.requirePermission("products:read", app.getUserHandler))
+
 	// Register routes with method, URL patterns, and handler functions
 	// app.requireActivatedUser
-
 	router.MethodFunc(http.MethodPost, "/v1/auth/register", app.registerUserHandler)
 	router.MethodFunc(http.MethodPatch, "/v1/auth/activate", app.activateUserHandler)
 	router.MethodFunc(http.MethodPost, "/v1/auth/me", app.createAuthenticationTokenHandler)
-	//api need by order service to get current user
-	router.MethodFunc(http.MethodGet, "/v1/users/{id}", app.requireActivatedUser(app.getUserHandler))
 
-	// Return the Chi router, which implements http.Handler
-	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
+	router.MethodFunc(http.MethodPut, "/v1/users/password", app.updateUserPasswordHandler)
+	router.MethodFunc(http.MethodPost, "/v1/tokens/activation", app.createActivationTokenHandler)
+	router.MethodFunc(http.MethodPost, "/v1/tokens/password-reset", app.createPasswordResetTokenHandler)
+
+	// Register a new GET /debug/vars endpoint pointing to the expvar handler for metric
+	router.Method(http.MethodGet, "/debug/vars", expvar.Handler())
+
+	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
 }
 
 // DELETE /v1/auth/logout              # Logout (delete tokens)
